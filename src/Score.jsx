d83b237029de2125aaa45c4152001d29e4246406
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-26 08:36:06 -0400

    Stub up Score and Student

---
+++ src/Score.jsx
@@ -0,0 +1,9 @@
+const Score = () => {
+  return (
+    <>
+    <h2>Student Scores</h2>
+    </>
+  )
+}
+
+export default Score
---
+++ src/Student.jsx
@@ -0,0 +1,12 @@
+import Score from "./Score"
+
+const Student = (props) => {
+  return ( 
+    <>
+    <h2>Students</h2>
+    <Score/>
+    </>
+  );
+}
+
+export default Student;
